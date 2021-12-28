aef404612847afce13aa368f872bdaf276b5f601
drakouliskal <drakouliskal@gmail.com>
2021-12-28 16:27:13 +0200

    Rebuilt Award and Individual Factories.

--- src/main/java/com/pfseven/smdb/smdb/bootstrap/AwardFactory.java
+++ src/main/java/com/pfseven/smdb/smdb/bootstrap/AwardFactory.java
@@ -1,13 +1,51 @@
 package com.pfseven.smdb.smdb.bootstrap;
 
 import com.pfseven.smdb.smdb.base.AbstractLogComponent;
+import com.pfseven.smdb.smdb.domain.Award;
+import com.pfseven.smdb.smdb.services.AwardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-public class AwardFactory extends AbstractLogComponent implements CommandLineRunner {
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Profile("content-generator")
+public class AwardsFactory extends AbstractLogComponent implements CommandLineRunner {
+
+    private final AwardService awardService;
 
     @Override
     public void run(String... args)  {
 
+        List<Award> awards = awardService.createAll(
+                Award.builder()
+                        .title("Best Picture")
+                        .dateOfAward('1994-11-11')
+                        .category("Oscar")
+                        .build(),
+                Award.builder()
+                        .title("Special Achievement Award")
+                        .dateOfAward('1996-02-20')
+                        .category("Oscar")
+                        .build(),
+                Award.builder()
+                        .title("Outstanding Animated Program")
+                        .dateOfAward('2018-01-12')
+                        .category("Emmy")
+                        .build()
+/*
+            //Form used for future inserts.
+                Award.builder()
+                        .title("")
+                        .dateOfAward('')
+                        .category("")
+                        .build(),
+*/
+        );
+        //awardService.createAll(awards);
+        logger.info("Created 3 awards: {}", awards);
     }
-
 }
--- src/main/java/com/pfseven/smdb/smdb/bootstrap/IndividualFactory.java
+++ src/main/java/com/pfseven/smdb/smdb/bootstrap/IndividualFactory.java
@@ -1,19 +1,122 @@
 package com.pfseven.smdb.smdb.bootstrap;
 
 import com.pfseven.smdb.smdb.base.AbstractLogComponent;
+import com.pfseven.smdb.smdb.domain.Individual;
+import com.pfseven.smdb.smdb.services.IndividualService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 @Profile("content-generator")
-public class IndividualFactory extends AbstractLogComponent implements CommandLineRunner {
+public class IndividualsFactory extends AbstractLogComponent implements CommandLineRunner {
+
+    private final IndividualService individualService;
 
     @Override
     public void run(String... args)  {
 
+        List<Individual> individual = individualService.createAll(
+                Individual.builder()
+                        .email("liamneeson@outlook.com")
+                        .age(69)
+                        .firstName("Liam")
+                        .lastName("Neeson")
+                        .nationality("British")
+                        .build(),
+                Individual.builder()
+                        .email("drewbarrymore@outlook.com")
+                        .age(46)
+                        .firstName("Drew")
+                        .lastName("Barrymore")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("timallen@gmail.com")
+                        .age(68)
+                        .firstName("Tim")
+                        .lastName("Allen")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("jonburton@gmail.com")
+                        .age(52)
+                        .firstName("Jon")
+                        .lastName("Burton")
+                        .nationality("British")
+                        .build(),
+                Individual.builder()
+                        .email("ronweasley@gmail.com")
+                        .age(60)
+                        .firstName("Wesley")
+                        .lastName("Archer")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("stevenspielrock@gmail.com")
+                        .age(77)
+                        .firstName("Steven")
+                        .lastName("Spielberg")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("melissamathison@gmail.com")
+                        .age(65)
+                        .firstName("Melissa")
+                        .lastName("Mathison")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("anndruchii@gmail.com")
+                        .age(73)
+                        .firstName("Ann")
+                        .lastName("Druyan")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("robertlevine@gmail.com")
+                        .age(45)
+                        .firstName("Robert")
+                        .lastName("Levine")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("nothisispatrick@gmail.com")
+                        .age(59)
+                        .firstName("Patrick")
+                        .lastName("Gilmore")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("stevenspielrock@gmail.com")
+                        .age(77)
+                        .firstName("Steven")
+                        .lastName("Spielberg")
+                        .nationality("American")
+                        .build(),
+                Individual.builder()
+                        .email("kathleenkennedy@gmail.com")
+                        .age(69)
+                        .firstName("Kathleen")
+                        .lastName("Kennedy")
+                        .nationality("American")
+                        .build()
+/*
+            //Form used for future inserts.
+                Individual.builder()
+                        .email("")
+                        .age()
+                        .firstName("")
+                        .lastName("")
+                        .nationality("")
+                        .build(),
+*/
+        );
+        //individualService.createAll(individuals);
+        logger.info("Created 12 Individuals: {}", individuals);
     }
-
 }
